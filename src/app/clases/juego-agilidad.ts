24a2860b8e308bcf3a84c3cd76e3f356328324ce
s3baRr00 <sebastianrilo@gmail.com>
2020-04-14 17:41:33 -0300

    estilos e imagenes cambiados

--- src/app/clases/juego-agilidad.ts
+++ src/app/clases/juego-agilidad.ts
@@ -1,4 +1,50 @@
-export class JuegoAgilidad {
-    public numeroIngresado;
-    public gano;
+import { Juego } from "../clases/juego";
+
+export class JuegoAgilidad extends Juego {
+  public operador;
+  public numeroUno;
+  public numeroDos;
+  public respuesta;
+  public respuestaIngresada;
+  public gano;
+
+  constructor(nombre?: string, gano?: boolean, jugador?: string)
+  {
+    super("Adivina el número", gano, jugador);
+  }
+  public verificar()
+  {
+    if (this.respuesta == this.respuestaIngresada) {
+      this.gano = true;
+    }
+    if (this.gano) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  public generarOperacion() {
+      let operadores = ['+','-','*','/'];
+      this.operador = operadores[this.aleatorio(0,3)];
+
+      this.numeroUno = this.aleatorio(0,100);
+      if(this.operador === '/')
+      {
+        this.numeroDos = this.aleatorio(1,this.numeroUno);
+      } 
+      else if(this.operador === '*')
+      {
+        this.numeroDos = this.aleatorio(0,this.numeroUno);
+      }
+      else{
+          this.numeroDos = this.aleatorio(0,100);
+      }
+      this.respuesta = eval(this.numeroUno + this.operador + this.numeroDos);
+  }
+
+    private aleatorio(a,b) 
+    {
+        return Math.round(Math.random()*(b-a)+a);
+    }
 }
--- src/app/componentes/listado/listado.component.ts
+++ src/app/componentes/listado/listado.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute, ParamMap } from '@angular/router';
 import { JuegoServiceService } from '../../servicios/juego-service.service';
 
 @Component({
