e551dd84551e04beaf1f50641b777b1394e983ff
s3baRr00 <sebastianrilo@gmail.com>
2020-05-05 18:01:01 -0300

    finalizado

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyBJsYJ5K43GOnfB7nVUekLy76aJDAa44Vk",
+    authDomain: "sala-de-juegos-angular.firebaseapp.com",
+    databaseURL: "https://sala-de-juegos-angular.firebaseio.com",
+    projectId: "sala-de-juegos-angular",
+    storageBucket: "sala-de-juegos-angular.appspot.com",
+    messagingSenderId: "414361427054",
+    appId: "1:414361427054:web:debf2a62f76649a6169a7f",
+    measurementId: "G-3N7ZJTY26P"
+  }
 };
